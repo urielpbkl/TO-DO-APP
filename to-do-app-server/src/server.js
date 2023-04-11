0c885ff4a9ac20009d76cc4a9d3c84659ff7cbc0
urielpbkl <58156383+urielpbkl@users.noreply.github.com>
2023-04-11 17:11:41 -0600

    signup y login implementados

--- to-do-app-server/src/server.js
+++ to-do-app-server/src/server.js
@@ -3,6 +3,8 @@ import express, { request, response } from 'express'
 import cors from 'cors'
 import { v4 as uuidv4 } from 'uuid';
 import { pool } from './db.js'
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const app = express()
 app.use(cors())
@@ -17,7 +19,9 @@ app.post('/add-new-todo', async (request, response) => {
     try {
         const newToDo = await pool.query(`INSERT INTO todos(id, user_email, title, progress, date) VALUES($1, $2, $3, $4, $5)`,
             [id, user_email, title, progress, date]) // DEBEN DE IR EN EL ORDEN DEL COMANDO SQL
-        response.json(newToDo)
+        response.json({
+            'message': 'agregado exitosamente'
+        })
     } catch (error) {
         console.log(error)
     }
@@ -50,8 +54,47 @@ app.put('/edit-todo/:id', async (request, response) => {
 app.delete('/delete-todo/:id', async (request, response) => {
     const { id } = request.params
     try {
-        const deleteToDo = await pool.query(`DELETE FROM todos WHERE id=$1;`,[id])
+        const deleteToDo = await pool.query(`DELETE FROM todos WHERE id=$1;`, [id])
         response.json(deleteToDo)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+// SIGNUP
+app.post('/sign-up', async (request, response) => {
+    const { email, password } = request.body
+    const salt = bcrypt.genSaltSync(10)
+    const hashedPassword = bcrypt.hashSync(password, salt)
+
+    try {
+        const signUp = await pool.query(`INSERT INTO users (email, hashed_password) VALUES($1, $2)`,
+            [email, hashedPassword])
+        const token = jwt.sign({ email }, 'secret', { expiresIn: '1hr' })
+
+        response.json({ email, token })
+
+    } catch (error) {
+        console.log(error)
+        error ? response.json({ detail: error.detail }) : ''
+    }
+
+})
+
+// LOGIN
+app.post('/login', async (request, response) => {
+    const { email, password } = request.body
+    try {
+        const users = await pool.query('SELECT * FROM users WHERE email = $1', [email])
+        !users.rows.length ? response.json({ detail: 'User doesn´t exist' }) : ''
+
+        const success = await bcrypt.compare(password, users.rows[0].hashed_password)
+        const token = jwt.sign({ email }, 'secret', { expiresIn: '1hr' })
+
+        success ?
+            response.json({ 'email': users.rows[0].email, token })
+            : response.json({ detail: 'login failed' })
+
     } catch (error) {
         console.log(error)
     }
